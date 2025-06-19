94a393c9c255ee508855e05d2c7b643ab95f67f7
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2025-06-19 10:44:08 +0200

    Ref + useImperativeHandle

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -1,14 +1,25 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import './App.css';
 import Select from './Select';
 
 function App() {
   const [item, setItem] = useState('Item 2');
+  const select2ApiRef = useRef(null);
 
   return (
     <>
       <h2>Demo Select (Rappels React !!!)</h2>
       <Select
+        items={['Item 1', 'Item 2', 'Item 3']}
+        value={item}
+        onChange={(item) => {
+          setItem(item);
+          select2ApiRef.current?.open();
+        }}
+        renderItem={(it) => (it === item ? <b>{it}</b> : it)}
+      />
+      <Select
+        ref={select2ApiRef}
         items={['Item 1', 'Item 2', 'Item 3']}
         value={item}
         onChange={(item) => setItem(item)}
--- hello-react/src/Select.tsx
+++ hello-react/src/Select.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useState } from 'react';
+import { forwardRef, ReactNode, use, useEffect, useImperativeHandle, useRef, useState } from 'react';
 import styles from './Select.module.css';
 
 type SelectProps = {
@@ -8,30 +8,58 @@ type SelectProps = {
   readonly renderItem?: (item: string) => ReactNode;
 };
 
+export interface SelectRef {
+  open: () => void;
+  close: () => void;
+  toggle: () => void;
+}
+
 // React.createElement(Select, {});
 
 /**
  * Select component for choosing an item from a list.
  */
-function Select({ items, value, onChange, renderItem }: SelectProps ): ReactNode {
-  console.log('SELECT');
+const Select = forwardRef<SelectRef, SelectProps>(
+  function Select({ items, value, onChange, renderItem }: SelectProps, ref ): ReactNode {
+  // console.log('SELECT');
 
-  useEffect(() => {
-    console.log('Effect');
-    
-  }, []);
+  // useEffect(() => {
+  //   console.log('Effect');
+  // }, []);
   
   const [menuOpen, setMenuOpen] = useState(false);
+  const hostRef = useRef<HTMLDivElement>(null);
+
+  useImperativeHandle(ref, () => ({
+    open: () => setMenuOpen(true),
+    close: () => setMenuOpen(false),
+    toggle: () => setMenuOpen(prev => !prev)
+  }));
+
+  // useEffect(() => {
+  //   // const menuEl = document.getElementById('menu');
+  //   console.log('Menu Element:', hostRef.current);
+  // });
+
+  useEffect(() => {
+    window.addEventListener('click', (event) => {
+      const hostEl = hostRef.current;
+      if (hostEl && !hostEl.contains(event.target as Node)) {
+        setMenuOpen(false);
+      }
+    });
+  }, []);
 
   return (
-    <div className={styles.Select}>
+    <div className={styles.Select} ref={hostRef}>
       <div className={styles.selected} onClick={() => setMenuOpen(!menuOpen)}>
         {value}
       </div>
       {menuOpen && (
-        <div className={styles.menu}>
+        <div id="menu" className={styles.menu}>
           {items.map(item => (
-            <div key={item} className={styles.menuItem} onClick={() => {
+            <div key={item} className={styles.menuItem} onClick={(event) => {
+              event.stopPropagation();
               onChange(item);
               setMenuOpen(false);
             }}>
@@ -42,6 +70,6 @@ function Select({ items, value, onChange, renderItem }: SelectProps ): ReactNode
       )}
     </div>
   )
-}
+});
 
 export default Select;
